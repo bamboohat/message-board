d0487ace4a34a62098d8f6a931a95f6a38ff5b02
bamboohat <shingoryu@gmail.com>
2015-07-13 06:49:38 +0000

    kadai age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,8 @@ class MessagesController < ApplicationController
   
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
+    #params.require(:message).permit(:name, :age)
   end
   
   def set_message
